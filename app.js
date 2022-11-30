fbb15d6621fce58769d76044f0279ab3a91528cb
Rif-7 <muhammadrifan974530@gmail.com>
2022-11-30 23:10:21 +0530

    add populatedb.js to test Schemas

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// Set up mongoose connection
+const mongoose = require("mongoose");
+
+const mongoDB = "mongodb+srv://testUser:testPasswordD1@cluster0.jft6vq8.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
