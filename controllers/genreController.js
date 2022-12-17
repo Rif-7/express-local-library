8646dbce810df13d9b896db2d0377462652bfa20
Rif-7 <muhammadrifan974530@gmail.com>
2022-12-17 22:37:58 +0530

    add genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,6 @@
 const Genre = require("../models/genre");
+const Book = require("../models/book");
+const async = require("async");
 
 
 // Display list of all Genre.
@@ -15,9 +17,37 @@ exports.genre_list = (req, res, next) => {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+exports.genre_detail = (req, res, next) => {
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+
+      genre_books(callback) {
+        Book.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        const err = new Error("Genre not found");
+        err.status = 404;
+        return next(err);
+      }
+      // Successful, so render
+      res.render("genre_detail", {
+        title: "Genre Detail",
+        genre: results.genre,
+        genre_books: results.genre_books,
+      });
+    }
+  );
 };
+
 
 // Display Genre create form on GET.
 exports.genre_create_get = (req, res) => {
