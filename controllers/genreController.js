90bb61ac421fc8a8541b8c89824b8215dde86f4a
Rif-7 <muhammadrifan974530@gmail.com>
2022-12-31 17:48:32 +0530

    add feat to delete genres

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -105,13 +105,65 @@ exports.genre_create_post = [
 
 // Display Genre delete form on GET.
 exports.genre_delete_get = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
+  async.parallel({
+    genre(callback) {
+      Genre.findById(req.params.id).exec(callback);
+    },
+    books(callback) {
+      Book.find({ genre: req.params.id }).exec(callback);
+    }
+  }, 
+  (err, results) => {
+    if (err) {
+      next(err);
+    }
+    if (results.genre == null) {
+      res.redirect("/catalog/genre");
+    }
+
+    res.render("genre_delete", {
+      title: "Delete Genre",
+      genre: results.genre,
+      genre_books: results.books
+    });
+
+
+  })
 };
 
 // Handle Genre delete on POST.
-exports.genre_delete_post = (req, res) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
-};
+exports.genre_delete_post = (req, res) => [
+  async.parallel({
+    genre(callback) {
+      Genre.findById(req.body.genreid).exec(callback);
+    },
+    books(callback) {
+      Book.find({ genre: req.body.genreid }).exec(callback);
+    }
+  },
+  (err, results) => {
+    if (err) {
+      next(err);
+    }
+    if (results.genre == null) {
+      res.redirect("/catalog/genre");
+    }
+    if (results.books.length > 0) {
+      res.render("genre_delete", {
+        title: "Delete Genre", 
+        genre: results.genre,
+        genre_books: results.books
+      });
+    }
+
+    Genre.findByIdAndRemove(req.body.genreid, (err, results) => {
+      if (err) {
+        next(err);
+      }
+      res.redirect("/catalog/genres");
+    });
+  })
+];
 
 // Display Genre update form on GET.
 exports.genre_update_get = (req, res) => {
