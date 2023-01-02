c8ad6eb224d04fd0cbaadf484a60152fd9ea00e5
Rif-7 <muhammadrifan974530@gmail.com>
2023-01-02 21:25:23 +0530

    add helmet and compression middlewares

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const compression = require("compression");
+const helmet = require('helmet');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var catalogRouter = require('./routes/catalog');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
 
-var app = express();
+const app = express();
 
 // Set up mongoose connection
 const mongoose = require("mongoose");
@@ -22,6 +24,9 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(compression());
+app.use(helmet());
 
 app.use(logger('dev'));
 app.use(express.json());
